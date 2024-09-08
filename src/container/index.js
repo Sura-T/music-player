e46598591b2d6bb97a30fe82f8bef94277470063
Sura-T <surafeltakele87@gmail.com>
2024-09-08 18:14:18 +0300

    Folder and file structured

---
+++ src/container/index.js
@@ -0,0 +1,4 @@
+export {default as Features} from './features/Feature';
+export {default as Footer} from './footer/Footer';
+export {default as Sidebar} from './sidebar/Sidebar';
+export {default as Header} from './header/Header';
